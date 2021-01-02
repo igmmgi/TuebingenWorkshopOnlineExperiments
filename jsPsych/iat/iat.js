9925b9c18963a207f89232ea74e14f464187a29e
ian <ian.mackenzie@uni-tuebingen.de>
2021-01-02 14:30:08 +0100

    updated iat task

--- jsPsych/iat/iat.js
+++ jsPsych/iat/iat.js
@@ -34,6 +34,12 @@
 // 4 abstract words: general, universal, abstract1, abstract2
 // 4 concrete words: specific1, specific2, detailed, defined
 
+// Just create two counter-balanced versions (see P614 paragraph 2)
+// Version 1:
+// 1st combined block: E real + abstract; I imaginary + concrete
+// Version 2:
+// 1st combined block: E imaginary + abstract; I real + concrete
+
 const expName = getFileName();
 const dirName = getDirName();
 const vpNum = genVpNum();
@@ -42,6 +48,7 @@ const vpNum = genVpNum();
 //                           Exp Parameters                           //
 ////////////////////////////////////////////////////////////////////////
 const prms = {
+  nBlks: 7,
   waitDur: 1000, // duration to wait following ...
   iti: 250, // inter-trial-interval
   respKeys: ["E", "I"],
@@ -53,20 +60,108 @@ const prms = {
 ////////////////////////////////////////////////////////////////////////
 //                      Welcome Instructionsi                         //
 ////////////////////////////////////////////////////////////////////////
-const task_instructions = {
+const task_instructions_general = {
   type: "html-keyboard-response",
   stimulus: generate_formatted_html({
-    text: `Welcome: <br>Your task is to ... <br><br>
-      Repond using the "E" and "I" keys. <br><br>
+    text: `Welcome: <br><br>Your task is to respond to single words presented in the centre of the screen according to word category.
+    You will respond using the "E" and "I" keys with your left and right index fingers, respectively. The word category and 
+    response key assignment will be indicated to you before each block of trials. Read the instructions carefully. Respond
+    as quickly and as accurately as possible! <br><br>
       Press the spacebar to continue!`,
     fontsize: 48,
     lineheight: 1.5,
     align: "left",
     bold: true,
     color: "blue",
+    width: "1800px",
   }),
   choices: prms.contKeys,
   post_trial_gap: prms.waitDur,
+};
+
+let nVersion = 1;
+let resp_key_block1;
+if (nVersion === 1) {
+  resp_key_e = ["real", "concrete"];
+  resp_key_i = ["imaginary", "abstract"];
+} else {
+  resp_key_e = ["imaginary", "concrete"];
+  resp_key_i = ["real", "abstract"];
+}
+
+const task_instructions_practice_block1 = {
+  type: "html-keyboard-response",
+  stimulus: generate_formatted_html({
+    text: `Block 1: <br><br>The following block consists of 16 training trials. Respond according the the word category: "real" vs. "imaginary" with the following key assignment:<br><br>
+      E key = "${resp_key_e[0]}"<br>I key = "${resp_key_i[0]}"
+      <br><br>Press the spacebar to continue!`,
+    fontsize: 48,
+    lineheight: 1.5,
+    align: "left",
+    bold: true,
+    color: "blue",
+    width: "1800px",
+  }),
+  choices: prms.contKeys,
+  post_trial_gap: prms.waitDur,
+};
+
+const task_instructions_practice_block2 = {
+  type: "html-keyboard-response",
+  stimulus: generate_formatted_html({
+    text: `Block 2: <br><br>The following block consists of 16 training trials. Respond according the the word category: "abstract" vs. "concrete" with the following key assignment:<br><br>
+      E key = "${resp_key_e[1]}"<br>I key = "${resp_key_i[1]}"
+      <br><br>Press the spacebar to continue!`,
+    fontsize: 48,
+    lineheight: 1.5,
+    align: "left",
+    bold: true,
+    color: "blue",
+    width: "1800px",
+  }),
+  choices: prms.contKeys,
+  post_trial_gap: prms.waitDur,
+};
+
+function blockFeedbackTxt(filter_options) {
+  "use strict";
+  let dat = jsPsych.data
+    .get()
+    .filter({ ...filter_options, blockNum: prms.cBlk });
+  // let nError = dat.select("error").values.filter(function (x) {
+  //   return x !== 0;
+  // }).length;
+
+  let nTotal = dat.count();
+  let nError = dat.select("error").sum();
+  let meanRT = dat.select("rt").mean();
+  let meanER = Math.round((nError / nTotal) * 100);
+
+  let blockFbTxt =
+    "<H1>Block: " +
+    prms.cBlk +
+    " of " +
+    prms.nBlks +
+    "</H1><br>" +
+    "<H1>Mean RT: " +
+    meanRT +
+    " ms </H1>" +
+    "<H1>Error Rate: " +
+    meanER +
+    " %</H1><br>" +
+    "<H2>Press any key to continue the experiment!</H2>";
+  prms.cBlk += 1;
+  return blockFbTxt;
+}
+
+const blk_feedback = {
+  type: "html-keyboard-response",
+  stimulus: "",
+  response_ends_trial: true,
+  post_trial_gap: prms.waitDur,
+  on_start: function (trial) {
+    trial.stimulus = blockFeedbackTxt({ stim: "iat" });
+  },
 };
 
 ////////////////////////////////////////////////////////////////////////
@@ -93,12 +188,32 @@ const stimuli = [
     {"word": "defined",   "category": "concrete"},
 ];
 
+const stimuli_block1 = stimuli.filter(function (stim) {
+  return stim.category == "real" || stim.category == "imaginary";
+});
+
+const stimuli_block2 = stimuli.filter(function (stim) {
+  return stim.category == "abstract" || stim.category == "concrete";
+});
+
 ////////////////////////////////////////////////////////////////////////
 //                          jsPsych Objects                           //
 ////////////////////////////////////////////////////////////////////////
 
 function codeTrial() {
-  let error = Math.random() < 0.5 ? 0 : 1;
+  let dat = jsPsych.data.get().last(1).values()[0];
+
+  let error = 0;
+  let key = jsPsych.pluginAPI.convertKeyCodeToKeyCharacter(dat.key_press);
+  if (key === "e") {
+    if (!resp_key_e.includes(dat.category)) {
+      error = 1;
+    }
+  } else if (key === "i") {
+    if (!resp_key_i.includes(dat.category)) {
+      error = 1;
+    }
+  }
 
   jsPsych.data.addDataToLastTrial({
     date: Date(),
@@ -140,18 +255,27 @@ const error_tone = {
 
 const error_instructions = {
   type: "html-keyboard-response",
-  stimulus: generate_formatted_html({
-    text: `Welcome: <br>Your task is to ... <br><br>
-      Repond using the "E" and "I" keys. <br><br>
-      Press the spacebar to continue!`,
-    fontsize: 48,
-    lineheight: 1.5,
-    align: "left",
-    bold: true,
-    color: "red",
-  }),
+  stimulus: "",
   post_trial_gap: prms.iti,
   choices: prms.contKeys,
+  on_start: function (trial) {
+    let txt;
+    if (prms.cBlk === 1) {
+      txt = `E key = "${resp_key_e[0]}"<br>I key = "${resp_key_i[0]}"<br><br>
+                      Press the spacebar to continue!`;
+    } else if (prms.cBlk === 2) {
+      txt = `E key = "${resp_key_e[1]}"<br>I key = "${resp_key_i[1]}"<br><br>
+                        Press the spacebar to continue!`;
+    }
+    trial.stimulus = generate_formatted_html({
+      text: txt,
+      fontsize: 48,
+      lineheight: 1.5,
+      align: "center",
+      bold: true,
+      color: "red",
+    });
+  },
 };
 
 const if_error = {
@@ -171,7 +295,6 @@ const iti = {
 
 const trial_timeline = {
   timeline: [iat_stimulus, if_error, iti],
-  timeline_variables: stimuli,
 };
 
 ////////////////////////////////////////////////////////////////////////
@@ -202,10 +325,33 @@ function genExpSeq() {
   exp.push(fullscreen_on);
   exp.push(hideMouseCursor);
   exp.push(welcome_en);
-
   // exp.push(vpInfoForm_en);
-  exp.push(task_instructions);
-  exp.push(trial_timeline);
+
+  // general instructions
+  exp.push(task_instructions_general);
+
+  let blk_timeline;
+
+  // block 1
+  exp.push(task_instructions_practice_block1);
+  blk_timeline = {
+    timeline: [iat_stimulus, if_error, iti],
+    timeline_variables: stimuli_block1,
+    randomize_order: true,
+    repetitions: 2,
+  };
+  exp.push(blk_timeline);
+  exp.push(blk_feedback);
+
+  // block 2
+  exp.push(task_instructions_practice_block2);
+  blk_timeline = {
+    timeline: [iat_stimulus, if_error, iti],
+    timeline_variables: stimuli_block2,
+    randomize_order: true,
+    repetitions: 2,
+  };
+  exp.push(blk_timeline);
 
   exp.push(save_data);
   exp.push(debrief_en);
@@ -218,5 +364,4 @@ const EXP = genExpSeq();
 
 jsPsych.init({
   timeline: EXP,
-  show_progress_bar: false,
 });
